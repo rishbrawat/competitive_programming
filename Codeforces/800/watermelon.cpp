53c7837c29b989e625b4ae8f296d967ffc3d5067
Rishaha <rishurawatx77@gmail.com>
2025-12-23 18:57:47 +0530

    codeforces watermelon solution.

---
+++ Codeforces/800/watermelon.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solution() {
+   int w;
+   cin >> w;
+   if (w > 2 && w % 2 == 0) {
+      cout << "YES";
+   }
+   else {
+      cout << "NO";
+   }
+}
+
+int main()
+{
+   solution();
+
+   return 0;
+}

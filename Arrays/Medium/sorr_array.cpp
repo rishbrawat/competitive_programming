609bb7420945b51cb1ef490ddece728621574e8b
Rishaha <rishurawatx77@gmail.com>
2026-01-23 17:03:47 +0530

    lc 912 soln

---
+++ Arrays/Medium/sorr_array.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(nums, 0, nums.size()-1);
+
+        return nums;
+    }
+    
+    void merge(vector<int>& nums, int left, int mid, int right) {
+        int n1 = mid-left+1;
+        int n2 = right-mid;
+
+        vector<int> l(n1), r(n2);
+
+        for(int i = 0; i < n1; ++i) {
+            l[i] = nums[left+i];
+        }
+
+        for(int j = 0; j < n2; ++j) {
+            r[j] = nums[mid+1+j];
+        }
+
+        int i = 0, j = 0, k = left;
+        while(i < n1 && j < n2) {
+            if(l[i] <= r[j]) {
+                nums[k++] = l[i++];
+            }
+            else {
+                nums[k++] = r[j++];
+            }
+        }
+        while(i < n1) {
+            nums[k++] = l[i++];
+        }
+        while(j < n2) {
+            nums[k++] = r[j++];
+        }
+    }
+
+
+    void mergeSort(vector<int>& nums, int left, int right) {
+        if(left >= right) return;
+
+        int mid = left + (right-left)/2;
+
+        mergeSort(nums, left, mid);
+        mergeSort(nums, mid+1, right);
+        merge(nums, left, mid, right);
+    }
+
+};
